8a087d535dbc079a82a4962ff53d61a0f52b1005
Brian Kahler <briankahler.designs@gmail.com>
2020-02-25 01:55:33 -0800

    My first personal python project. A simple calculator that tracks wins and loses with user imputs and gives avrg of total games played with a restart.

---
+++ csgo_calc v0.02.py
@@ -0,0 +1,22 @@
+wins = 0
+loses = 0
+count = 0
+
+while True:
+  track = input('Enter "win" or "lose". Enter "done" when finished. ')
+  if count != 'done':
+    count += 1
+  if track == 'win':
+    wins += 1
+  elif track == 'lose':
+    loses += 1
+  elif track == 'done':
+    print('You won ', wins, 'times')
+    print('You lost ', loses, 'times') 
+    def average(wins, loses):
+      return (wins + loses) / 2
+    avg = average(wins, loses)
+    print('your average today was: ',avg, "in", count -1, 'games!')
+    if input('Do you want to start again y/n: ') != 'y':
+      break
+ 
