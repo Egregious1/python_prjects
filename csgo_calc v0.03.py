230ad8d8e68eae37b09e3ed52788e0760bc9c08c
Brian Kahler <briankahler.designs@gmail.com>
2020-03-04 01:06:17 -0800

    god i feel like i fucked up the code here. I tried adding a map selection at the start and must have deleted something vital for the average counter because now its fucked. I should have just gone to sleep

--- csgo_calc v0.03.py
+++ csgo_calc v0.03.py
@@ -3,37 +3,50 @@ loses = 0
 count = 0
 ovr_wins = []
 ovr_loses = []
-print('|========== CS:GO Calculator ==========|') # made for simple tracking for overall win/lose ratios.
+maps = ['dust2', 'inferno', 'train', 'mirage', 'nuke', 'overpass', 'vertigo']
+
+print('|========== CS:GO Calculator ==========|')  # made for simple tracking for overall win/lose ratios.
 print('')
-user_name = input('Please enter a username: ') # gets printed when entering done
+user_name = input('Please enter a username: ')  # gets printed when entering done
+while True:
+    map_name = str(input('What map are you playing? '))
+    if map_name in maps:
+        print('Thank you!')
+        break
+    elif map_name not in maps:
+        print('Please enter valid map!')
+
 while True:
     track = input('Enter "win" or "lose". Enter "done" when finished. ')
     if track not in {'win', 'lose', 'done'}:
-      print('!===== Not a valid entry =====!')
-      print('')
+        print('!===== Not a valid entry =====!')
+        print('')
     if track == 'win':
         wins += 1
         count += 1
         ovr_wins.append(track)
+
     elif track == 'lose':
         loses += 1
         count += 1
         ovr_loses.append(track)
-    elif track == 'done': # wrap up returns games won/lost and an overall average
+
+    elif track == 'done':  # wrap up returns games won/lost and an overall average
         print('')
-        print(user_name + ' this time:')
+        print(user_name + ' this time on ' + map_name + ':')
         print('You won ', wins, 'games')
         print('You lost ', loses, 'games')
         wins = 0
         loses = 0
 
-        def average(ovr_wins, ovr_loses): # used to find average between both counts
-            return (ovr_loses + ovr_wins) / 2
+        def average(ovr_wins, ovr_loses):  # used to find average between both counts it stopped working......what the flying fuck!
+            return (loses + wins) / 2
 
         avg = average(wins, loses)
         print('your average was: ', avg, "in", count, 'games!')
         print('')
-        if input('Do you want to start again y/n: ') != 'y': # anything other than y stops program.
-          print('')
-          print('|========== Restart Program ===========|')
-          break
+        if input('Do you want to start again y/n: ') != 'y':  # anything other than y stops program.
+            print('')
+            print('|========== Restart Program ===========|')
+            break
+
